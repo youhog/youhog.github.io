71f5a2d1d527320f98e36f3dd2d5de62808d0112
Jiziyan1211 <jiziyan440359@gmail.com>
2025-12-26 16:52:19 +0800

    1226_test7

--- wp-content/plugins/essential-blocks/src/blocks/advanced-tabs/src/constants/typographyPrefixConstants.js
+++
@@ -1,4 +0,0 @@
-// each and every const here has to be totally unique from one another
-
-export const typoPrefixTabTitle = "title_";
-export const typoPrefixTabSubtitle = "subtitle_";
--- wp-content/plugins/essential-blocks/src/blocks/flipbox/src/constants/borderShadowConstants.js
+++
@@ -1,6 +0,0 @@
-export const borderShadow = "front_";
-export const borderShadowBtn = "btn_";
-export const borderShadowFrontIcon = "front_icon_";
-export const borderShadowBackIcon = "back_icon_";
-export const frontImageBorder = "front_img";
-export const backImageBorder = "back_img";
